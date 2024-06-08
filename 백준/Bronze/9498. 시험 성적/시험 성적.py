e2c5889ecb095bd88f7a3c8129d265e13a7f95d7
HaeSeong Kim <stevenkim1259@gmail.com>
2024-06-08 15:23:02 +0900

    [Bronze V] Title: 시험 성적, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+A = int(input())
+
+if 90 <= A and A <= 100:
+    print("A")
+elif 80 <= A and A < 90:
+    print("B")
+elif 70 <= A and A < 80:
+    print("C")
+elif 60 <= A and A < 70:
+    print("D")
+else:
+    print("F")
