3e0ffcec3c730d83619b1ea7091fe5005fcb9a02
HaeSung Kim <stevenkim1259@gmail.com>
2024-07-30 15:14:51 +0900

    [Bronze II] Title: Hashing, Time: 112 ms, Memory: 108080 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,13 @@
+L = int(input())
+arr = list(input())
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+sum = 0
+cnt = 0
+
+for i in arr:
+    idx = alphabet.index(i)+1
+    sum += idx * (31**cnt)
+    cnt += 1
+    
+print(sum)
